1fc8d49a6fb8c6fcd4c41a4a30422394cd0fb083
jfonte0512 <jfonte0512@starkstate.net>
2024-01-27 16:21:13 -0500

    Week 2 Lab

---
+++ Fonte-Week2Lab/js/plants.js
@@ -0,0 +1,14 @@
+/*
+	JavaScript ~ WDD221
+	Week 1 ~ Guided Activity ~ Working with Events and Elements 
+	Web Site for a fictional company ~ Tinley Xeriscapes
+	Plants web page
+	Author: kmison
+	Date:	2018
+	Filenames: plants.htm, tinley.css, tinleyhand.css
+*/
+
+//define variables containing img src values
+let blanket = "images/blanket.jpg";
+let bluestem = "images/bluestem.jpg";
+let rugosa = "images/rugosa.jpg";
