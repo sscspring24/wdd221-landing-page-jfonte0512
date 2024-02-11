0eaa6f6f380f0f30b6c0ee6be0e7a9d790cdf9c6
jfonte0512 <jfonte0512@starkstate.net>
2024-02-11 10:31:04 -0500

    Week 3 Lab

---
+++ Fonte_Week3Lab/js/dogyears.js
@@ -0,0 +1,25 @@
+//store the value of my current age
+const myAge = 40;
+//This is my current age and it won't change, which is why it is a const!
+
+//store the value of 2
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//you finish the rest!
+
+//calculate years
+
+//calc dog years
+let laterYears = myAge -2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+//add years
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Jonathan' .toLowerCase();
+
+console.log('My name is '+ myName + ' . I am ' + myAgeInDogYears + ' years old in dogs years.');
+document.write('My name is '+ myName + ' . I am ' + myAgeInDogYears + ' years old in dogs years.');
+//print to the console values stored in variables
